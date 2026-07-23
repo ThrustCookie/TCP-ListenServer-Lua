f4b4653f9df115112ab1a094048f3ffd86e44c97
Cemi <cemimedina@icloud.com>
2026-07-22 23:16:21 -0400

    tried adding compression to the packets
    this isn't working correctly, I'm assuming the compression is accidentally making a '\n' character which caused the msg to get cut off, this makes it fail when decompressing

--- main.lua
+++ main.lua
@@ -11,93 +11,70 @@
 local socket = require('socket')
 local address, port = "localhost", 12345
 
-local conn_type
-local world
+local conn_type ---@type 'client' | 'server' | nil
 local tcp
 
 local server = {}
 local client = {}
+local connected
+
+local msg = 'Hello World!Hello World!Hello World!Hello World!Hello World!123 123 123 123 123 123'
+local compressed
+local decompressed
+
+local format = 'zlib'
 
 function love.load()
     tcp = socket.tcp()
-    world = {
-        server = { x = 100, y = 100, },
-        client = { x = 400, y = 100, },
-    }
 end
 
-function love.keypressed( key, isrepeat )
-    if key == 'c' then -- client
-        client.load()
-        conn_type = 'client'
-        
-    elseif key == 's' then -- server
-        server.load()
-        conn_type = 'server'
-    end
-end
+local function send_msg()
+    if not connected then return end
 
-local function split(s, delimiter)
-	local result = {}
-	for match in (s..delimiter):gmatch("(.-)"..delimiter) do
-		result[#result+1] = match
-	end
-	return result
+    compressed = love.data.compress('string', format, msg, 1)
+    connected:send(conn_type..": "..compressed..'\n')
 end
-
 
 function client.load()
     assert(tcp:connect(address, port))
     print("Connected to: ", tcp:getpeername())
     tcp:settimeout(0)
-end
-function client.update()
-    world.client.x, world.client.y = love.mouse.getPosition()
-    local pos = string.format("%d-%d\n", world.client.x, world.client.y)
-	tcp:send(pos)
-
-	local data, error, partial = tcp:receive()
-    if error then assert(error == "timeout", error, partial) end
-	if data then
-		local p = split(data, '-')
-		world.server.x, world.server.y = p[1], p[2]
-	end
+    connected = tcp
 end
 
 function server.load()
     assert(tcp:bind(address, port))
     assert(tcp:listen(1))
     print("Server Started", tcp:getsockname())
-
     tcp:settimeout(0)
 end
-function server.update()
-    if server.client == nil then --- looks for connection
-        local error
-        server.client, error = tcp:accept()
-        if error and error ~= 'timeout' then print(error) end
-        if server.client then
-            server.client:settimeout(0)
+
+local function look_for_connection()
+    local error
+    connected, error = tcp:accept()
+    if error and error ~= 'timeout' then print(error) end
+
+    if connected then
+        connected:settimeout(0)
+    end
+end
+
+local function socket_check_data()
+    local data, error, partial = connected:receive()
+
+    if error and error ~= 'timeout' then
+        if error == 'closed' then
+            print('disconected')
+            connected = nil
+        else
+            print(error, partial)
         end
         return
     end
-    world.server.x, world.server.y = love.mouse.getPosition()
-    local pos = string.format("%d-%d\n", world.server.x, world.server.y)
-	server.client:send(pos)
-
-	local data, error, partial = server.client:receive()
-    if error and error ~= 'timeout' then
-        if error == 'closed' then
-            print('client disconected')
-            server.client = nil
-            return
-        end
-        print(error, partial)
-    end
 
 	if data then
-		local p = split(data, '-')
-		world.client.x, world.client.y = p[1], p[2]
+		print(data)
+        decompressed = love.data.decompress('string', format, data)
 	end
 end
 
@@ -106,25 +83,46 @@ local t = 0
 function love.update(dt)
     t = t + dt
     if t > update_rate then
-        if conn_type == 'client' then
-            client.update()
-        elseif conn_type == 'server' then
-            server.update()
+        if connected then
+            socket_check_data()
+
+        elseif not connected then
+            if conn_type == 'server' then
+                look_for_connection()
+            end
+        else
+            
         end
         t = t - update_rate
     end
 end
 
 function love.draw()
-	love.graphics.setColor(0, 1, 0)
-	love.graphics.rectangle("fill", world.server.x, world.server.y, 50, 50)
+    for i, value in pairs({
+        conn_type,
+        msg,
+        decompressed,
+    }) do
+        if value then
+            love.graphics.print(value,0,15*i)
+        end
+    end
+end
 
-	love.graphics.setColor(1, 0, 0)
-	love.graphics.rectangle("fill", world.client.x, world.client.y, 50, 50)
-    
-	love.graphics.setColor(1, 1, 1)
-    if conn_type then
-        love.graphics.print(conn_type,0,0)
+function love.keypressed( key, isrepeat )
+    if not conn_type then
+        if key == 'c' then -- client
+            client.load()
+            conn_type = 'client'
+            
+        elseif key == 's' then -- server
+            server.load()
+            conn_type = 'server'
+        end
+        return
+    end
+    if key == 'return' then
+        send_msg()
     end
 end
 
